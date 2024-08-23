facecb34873ba3c9e1bcea7998d0285e1a80e88b
osama-zayed <o.a.s775561590@gmail.com>
2024-08-23 03:44:26 +0300

    Update the product card

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,56 +19,78 @@ if (carousel) {
     draggable: true, // Activation du déplacement manuel
   });
 
-    // Pause lors du survol
-    carousel.addEventListener("mouseenter", () => {
-      flkty.stopPlayer();
-    });
-    carousel.addEventListener("mouseleave", () => {
-      flkty.playPlayer();
-    });
-  }
-
-  const mainCarousel = document.querySelector(".main-carousel");
-  if (mainCarousel) {
-    const flkty = new Flickity(mainCarousel, {
-      contain: true,
-      wrapAround: true,
-      pageDots: false,
-      prevNextButtons: false,
-      autoPlay: 2000, // Lecture automatique toutes les 5 secondes
-      draggable: true, // Activation du déplacement manuel
-    });
-
-    // Pause lors du survol
-    mainCarousel.addEventListener("mouseenter", () => {
-      flkty.stopPlayer();
-    });
-    mainCarousel.addEventListener("mouseleave", () => {
-      flkty.playPlayer();
-    });
-  }
-
-  // Hide navigation icons
-  const flickityPrevNextButtons = document.querySelectorAll(".flickity-button");
-  flickityPrevNextButtons.forEach(button => {
-    button.style.display = "none";
+  // Pause lors du survol
+  carousel.addEventListener("mouseenter", () => {
+    flkty.stopPlayer();
   });
-  // إظهار الزر عند التمرير لأسفل الصفحة
-  window.onscroll = function () {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      scrollToTopBtn.classList.add("show");
+  carousel.addEventListener("mouseleave", () => {
+    flkty.playPlayer();
+  });
+}
+
+const mainCarousel = document.querySelector(".main-carousel");
+if (mainCarousel) {
+  const flkty = new Flickity(mainCarousel, {
+    contain: true,
+    wrapAround: true,
+    pageDots: false,
+    prevNextButtons: false,
+    autoPlay: 2000, // Lecture automatique toutes les 5 secondes
+    draggable: true, // Activation du déplacement manuel
+  });
+
+  // Pause lors du survol
+  mainCarousel.addEventListener("mouseenter", () => {
+    flkty.stopPlayer();
+  });
+  mainCarousel.addEventListener("mouseleave", () => {
+    flkty.playPlayer();
+  });
+}
+
+// Hide navigation icons
+const flickityPrevNextButtons = document.querySelectorAll(".flickity-button");
+flickityPrevNextButtons.forEach(button => {
+  button.style.display = "none";
+});
+// إظهار الزر عند التمرير لأسفل الصفحة
+window.onscroll = function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopBtn.classList.add("show");
+  } else {
+    scrollToTopBtn.classList.remove("show");
+  }
+};
+
+// التمرير إلى أعلى الصفحة عند الضغط على الزر
+document.getElementById("scrollToTopBtn").onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // تأثير تمرير سلس
+  });
+};
+
+window.addEventListener('load', function () {
+  var loadingScreen = document.getElementById('loading-screen');
+
+  function hideLoadingScreen() {
+    loadingScreen.style.display = 'none';
+  }
+
+  function showLoadingScreen() {
+    loadingScreen.style.display = 'flex';
+  }
+
+  function checkInternetConnection() {
+    if (!navigator.onLine) {
+      showLoadingScreen();
+      setTimeout(hideLoadingScreen, 3000);
+      setTimeout(checkInternetConnection, 1000);
     } else {
-      scrollToTopBtn.classList.remove("show");
+      hideLoadingScreen();
     }
-  };
+  }
 
-  // التمرير إلى أعلى الصفحة عند الضغط على الزر
-  document.getElementById("scrollToTopBtn").onclick = function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth" // تأثير تمرير سلس
-    });
-  };
-
-  
+  checkInternetConnection();
+});
